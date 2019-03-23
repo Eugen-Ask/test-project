ff9aa8cdb86f08a9d4354cc41468dc917b76dcd8
Eugene Beliaev <eugenbelly@gmail.com>
2019-03-23 12:27:31 +0300

    Remove dead code

--- src/app/App.js
+++ src/app/App.js
@@ -1,42 +1,10 @@
 import React from 'react'
-import emotion from '@emotion/styled/macro'
-import IntersectionObserver from '@researchgate/react-intersection-observer'
 
 import { SearchBar } from './SearchBar'
 import { Assignees } from './Assignees'
 import { Issues } from './Issues'
 
 export class App extends React.PureComponent {
-  LoadMoreIssuesTrigger = () => {
-    const { app, loading } = this.props
-    if (
-      app.issues.data.length > 0 
-      && app.issues.lastLoadedPage < app.issues.totalPages
-      && !loading.loadIssues
-      && !loading.loadRepository
-    ){
-      return (
-        <IntersectionObserver
-          children={<div/>}
-          onChange={({ isIntersecting }) => {
-            if (isIntersecting) this.props.loadIssues()
-          }}
-        />
-      )
-    }
-    return null
-  }
-  
-  get assignees() {
-    const { assignees, assigneeSearchInputValue } = this.props.app
-    return assignees.data
-      .filter(_ =>
-        _.login
-          .toLowerCase()
-          .startsWith(assigneeSearchInputValue.toLowerCase())
-      )
-  }
-  
   render() {
     const { app } = this.props
     return (
